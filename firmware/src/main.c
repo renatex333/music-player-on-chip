a4123b0ce0f353e9fd9fee70c8249c4aef488f26
Renato Laffranchi Falcão <renatolf1@al.insper.edu.br>
2022-09-05 18:34:01 -0300

    Criando Structs e alterando função main

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -20,7 +20,7 @@
 #define SELECAO_PIO_IDX_MASK (1u << SELECAO_PIO_IDX)
 
 
-/* Meteção de loko */
+/* Definir as notas */
 #define NOTE_B0  31
 #define NOTE_C1  33
 #define NOTE_CS1 35
@@ -126,105 +126,185 @@ void buzzer_test(int);
 void tone(int, int);
 
 /************************************************************************/
-/* variables                                                           */
+/* Criando as structs                                                   */
 /************************************************************************/
 
-
-// notes of the moledy followed by the duration.
-// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
-// !!negative numbers are used to represent dotted notes,
-// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
-int melody[] = {
-
-	// Super Mario Bros theme
-	// Score available at https://musescore.com/user/2123/scores/2145
-	// Theme by Koji Kondo
+typedef struct {
 	
+	// change this to make the song slower or faster
+	int tempo;
 	
-	NOTE_E5,8, NOTE_E5,8, REST,8, NOTE_E5,8, REST,8, NOTE_C5,8, NOTE_E5,8, //1
-	NOTE_G5,4, REST,4, NOTE_G4,8, REST,4,
-	NOTE_C5,-4, NOTE_G4,8, REST,4, NOTE_E4,-4, // 3
-	NOTE_A4,4, NOTE_B4,4, NOTE_AS4,8, NOTE_A4,4,
-	NOTE_G4,-8, NOTE_E5,-8, NOTE_G5,-8, NOTE_A5,4, NOTE_F5,8, NOTE_G5,8,
-	REST,8, NOTE_E5,4,NOTE_C5,8, NOTE_D5,8, NOTE_B4,-4,
-	NOTE_C5,-4, NOTE_G4,8, REST,4, NOTE_E4,-4, // repeats from 3
-	NOTE_A4,4, NOTE_B4,4, NOTE_AS4,8, NOTE_A4,4,
-	NOTE_G4,-8, NOTE_E5,-8, NOTE_G5,-8, NOTE_A5,4, NOTE_F5,8, NOTE_G5,8,
-	REST,8, NOTE_E5,4,NOTE_C5,8, NOTE_D5,8, NOTE_B4,-4,
-
+	// notes of the moledy followed by the duration.
+	// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+	// !!negative numbers are used to represent dotted notes,
+	// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+	int melody[];
 	
-	REST,4, NOTE_G5,8, NOTE_FS5,8, NOTE_F5,8, NOTE_DS5,4, NOTE_E5,8,//7
-	REST,8, NOTE_GS4,8, NOTE_A4,8, NOTE_C4,8, REST,8, NOTE_A4,8, NOTE_C5,8, NOTE_D5,8,
-	REST,4, NOTE_DS5,4, REST,8, NOTE_D5,-4,
-	NOTE_C5,2, REST,2,
+} song;
 
-	REST,4, NOTE_G5,8, NOTE_FS5,8, NOTE_F5,8, NOTE_DS5,4, NOTE_E5,8,//repeats from 7
-	REST,8, NOTE_GS4,8, NOTE_A4,8, NOTE_C4,8, REST,8, NOTE_A4,8, NOTE_C5,8, NOTE_D5,8,
-	REST,4, NOTE_DS5,4, REST,8, NOTE_D5,-4,
-	NOTE_C5,2, REST,2,
 
-	NOTE_C5,8, NOTE_C5,4, NOTE_C5,8, REST,8, NOTE_C5,8, NOTE_D5,4,//11
-	NOTE_E5,8, NOTE_C5,4, NOTE_A4,8, NOTE_G4,2,
+song jigglypuf;
+jigglypuf.tempo = 85;
 
-	NOTE_C5,8, NOTE_C5,4, NOTE_C5,8, REST,8, NOTE_C5,8, NOTE_D5,8, NOTE_E5,8,//13
-	REST,1,
-	NOTE_C5,8, NOTE_C5,4, NOTE_C5,8, REST,8, NOTE_C5,8, NOTE_D5,4,
-	NOTE_E5,8, NOTE_C5,4, NOTE_A4,8, NOTE_G4,2,
-	NOTE_E5,8, NOTE_E5,8, REST,8, NOTE_E5,8, REST,8, NOTE_C5,8, NOTE_E5,4,
-	NOTE_G5,4, REST,4, NOTE_G4,4, REST,4,
-	NOTE_C5,-4, NOTE_G4,8, REST,4, NOTE_E4,-4, // 19
+jigglypuf.melody = {
+
+	// Jigglypuff's Song
+	// Score available at https://musescore.com/user/28109683/scores/5044153
 	
-	NOTE_A4,4, NOTE_B4,4, NOTE_AS4,8, NOTE_A4,4,
-	NOTE_G4,-8, NOTE_E5,-8, NOTE_G5,-8, NOTE_A5,4, NOTE_F5,8, NOTE_G5,8,
-	REST,8, NOTE_E5,4, NOTE_C5,8, NOTE_D5,8, NOTE_B4,-4,
-
-	NOTE_C5,-4, NOTE_G4,8, REST,4, NOTE_E4,-4, // repeats from 19
-	NOTE_A4,4, NOTE_B4,4, NOTE_AS4,8, NOTE_A4,4,
-	NOTE_G4,-8, NOTE_E5,-8, NOTE_G5,-8, NOTE_A5,4, NOTE_F5,8, NOTE_G5,8,
-	REST,8, NOTE_E5,4, NOTE_C5,8, NOTE_D5,8, NOTE_B4,-4,
-
-	NOTE_E5,8, NOTE_C5,4, NOTE_G4,8, REST,4, NOTE_GS4,4,//23
-	NOTE_A4,8, NOTE_F5,4, NOTE_F5,8, NOTE_A4,2,
-	NOTE_D5,-8, NOTE_A5,-8, NOTE_A5,-8, NOTE_A5,-8, NOTE_G5,-8, NOTE_F5,-8,
+	NOTE_D5,-4, NOTE_A5,8, NOTE_FS5,8, NOTE_D5,8,
+	NOTE_E5,-4, NOTE_FS5,8, NOTE_G5,4,
+	NOTE_FS5,-4, NOTE_E5,8, NOTE_FS5,4,
+	NOTE_D5,-2,
+	NOTE_D5,-4, NOTE_A5,8, NOTE_FS5,8, NOTE_D5,8,
+	NOTE_E5,-4, NOTE_FS5,8, NOTE_G5,4,
+	NOTE_FS5,-1,
+	NOTE_D5,-4, NOTE_A5,8, NOTE_FS5,8, NOTE_D5,8,
+	NOTE_E5,-4, NOTE_FS5,8, NOTE_G5,4,
 	
-	NOTE_E5,8, NOTE_C5,4, NOTE_A4,8, NOTE_G4,2, //26
-	NOTE_E5,8, NOTE_C5,4, NOTE_G4,8, REST,4, NOTE_GS4,4,
-	NOTE_A4,8, NOTE_F5,4, NOTE_F5,8, NOTE_A4,2,
-	NOTE_B4,8, NOTE_F5,4, NOTE_F5,8, NOTE_F5,-8, NOTE_E5,-8, NOTE_D5,-8,
-	NOTE_C5,8, NOTE_E4,4, NOTE_E4,8, NOTE_C4,2,
-
-	NOTE_E5,8, NOTE_C5,4, NOTE_G4,8, REST,4, NOTE_GS4,4,//repeats from 23
-	NOTE_A4,8, NOTE_F5,4, NOTE_F5,8, NOTE_A4,2,
-	NOTE_D5,-8, NOTE_A5,-8, NOTE_A5,-8, NOTE_A5,-8, NOTE_G5,-8, NOTE_F5,-8,
+	NOTE_FS5,-4, NOTE_E5,8, NOTE_FS5,4,
+	NOTE_D5,-2,
+	NOTE_D5,-4, NOTE_A5,8, NOTE_FS5,8, NOTE_D5,8,
+	NOTE_E5,-4, NOTE_FS5,8, NOTE_G5,4,
+	NOTE_FS5,-1,
 	
-	NOTE_E5,8, NOTE_C5,4, NOTE_A4,8, NOTE_G4,2, //26
-	NOTE_E5,8, NOTE_C5,4, NOTE_G4,8, REST,4, NOTE_GS4,4,
-	NOTE_A4,8, NOTE_F5,4, NOTE_F5,8, NOTE_A4,2,
-	NOTE_B4,8, NOTE_F5,4, NOTE_F5,8, NOTE_F5,-8, NOTE_E5,-8, NOTE_D5,-8,
-	NOTE_C5,8, NOTE_E4,4, NOTE_E4,8, NOTE_C4,2,
-	NOTE_C5,8, NOTE_C5,4, NOTE_C5,8, REST,8, NOTE_C5,8, NOTE_D5,8, NOTE_E5,8,
-	REST,1,
-
-	NOTE_C5,8, NOTE_C5,4, NOTE_C5,8, REST,8, NOTE_C5,8, NOTE_D5,4, //33
-	NOTE_E5,8, NOTE_C5,4, NOTE_A4,8, NOTE_G4,2,
-	NOTE_E5,8, NOTE_E5,8, REST,8, NOTE_E5,8, REST,8, NOTE_C5,8, NOTE_E5,4,
-	NOTE_G5,4, REST,4, NOTE_G4,4, REST,4,
-	NOTE_E5,8, NOTE_C5,4, NOTE_G4,8, REST,4, NOTE_GS4,4,
-	NOTE_A4,8, NOTE_F5,4, NOTE_F5,8, NOTE_A4,2,
-	NOTE_D5,-8, NOTE_A5,-8, NOTE_A5,-8, NOTE_A5,-8, NOTE_G5,-8, NOTE_F5,-8,
-	
-	NOTE_E5,8, NOTE_C5,4, NOTE_A4,8, NOTE_G4,2, //40
-	NOTE_E5,8, NOTE_C5,4, NOTE_G4,8, REST,4, NOTE_GS4,4,
-	NOTE_A4,8, NOTE_F5,4, NOTE_F5,8, NOTE_A4,2,
-	NOTE_B4,8, NOTE_F5,4, NOTE_F5,8, NOTE_F5,-8, NOTE_E5,-8, NOTE_D5,-8,
-	NOTE_C5,8, NOTE_E4,4, NOTE_E4,8, NOTE_C4,2,
-	
-	//game over sound
-	NOTE_C5,-4, NOTE_G4,-4, NOTE_E4,4, //45
-	NOTE_A4,-8, NOTE_B4,-8, NOTE_A4,-8, NOTE_GS4,-8, NOTE_AS4,-8, NOTE_GS4,-8,
-	NOTE_G4,8, NOTE_D4,8, NOTE_E4,-2,
-
 };
+
+song nevergonnagiveyouup;
+nevergonnagiveyouup.tempo = 114;
+
+nevergonnagiveyouup.melody = {
+
+	// Never Gonna Give You Up - Rick Astley
+	// Score available at https://musescore.com/chlorondria_5/never-gonna-give-you-up_alto-sax
+	// Arranged by Chlorondria
+
+	NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,4, //1
+	NOTE_E5,-4, NOTE_FS5,-4, NOTE_A5,16, NOTE_G5,16, NOTE_FS5,8,
+	NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,2,
+	NOTE_A4,16, NOTE_A4,16, NOTE_B4,16, NOTE_D5,8, NOTE_D5,16,
+	NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,4, //repeat from 1
+	NOTE_E5,-4, NOTE_FS5,-4, NOTE_A5,16, NOTE_G5,16, NOTE_FS5,8,
+	NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,2,
+	NOTE_A4,16, NOTE_A4,16, NOTE_B4,16, NOTE_D5,8, NOTE_D5,16,
+	REST,4, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_D5,8, NOTE_E5,8, NOTE_CS5,-8,
+	NOTE_B4,16, NOTE_A4,2, REST,4,
+
+	REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,4, NOTE_A4,8, //7
+	NOTE_A5,8, REST,8, NOTE_A5,8, NOTE_E5,-4, REST,4,
+	NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, NOTE_D5,8, NOTE_E5,8, REST,8,
+	REST,8, NOTE_CS5,8, NOTE_B4,8, NOTE_A4,-4, REST,4,
+	REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, NOTE_A4,4,
+	NOTE_E5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,4, REST,4,
+	
+	NOTE_D5,2, NOTE_E5,8, NOTE_FS5,8, NOTE_D5,8, //13
+	NOTE_E5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,4, NOTE_A4,4,
+	REST,2, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8,
+	REST,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+	NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+
+	NOTE_E5,-8, NOTE_E5,-8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,-8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16, //18
+	NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,8, NOTE_A4,8, NOTE_A4,8,
+	NOTE_E5,4, NOTE_D5,2, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+	NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+	NOTE_A5,4, NOTE_CS5,8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+
+	NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8,  //23
+	NOTE_E5,4, NOTE_D5,2, REST,4,
+	REST,8, NOTE_B4,8, NOTE_D5,8, NOTE_B4,8, NOTE_D5,8, NOTE_E5,4, REST,8,
+	REST,8, NOTE_CS5,8, NOTE_B4,8, NOTE_A4,-4, REST,4,
+	REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, NOTE_A4,4,
+	REST,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8, NOTE_D5,8,
+	
+	REST,8, NOTE_A4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, //29
+	REST,8, NOTE_CS5,8, NOTE_B4,8, NOTE_A4,-4, REST,4,
+	NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, NOTE_A4,4, REST,8,
+	REST,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,4, NOTE_E5,-4,
+	NOTE_D5,2, NOTE_D5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,4,
+	NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8, NOTE_A4,8, NOTE_A4,4,
+
+	REST,-4, NOTE_A4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, //35
+	REST,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+	NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+	NOTE_E5,-8, NOTE_E5,-8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+	NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8,
+
+	NOTE_E5,4, NOTE_D5,2, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16, //40
+	NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+	NOTE_A5,4, NOTE_CS5,8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+	NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8,
+	NOTE_E5,4, NOTE_D5,2, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+	
+	NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16, //45
+	NOTE_A5,4, NOTE_CS5,8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+	NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8,
+	NOTE_E5,4, NOTE_D5,2, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+	NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16, //45
+	
+	NOTE_A5,4, NOTE_CS5,8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+	NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8,
+
+	NOTE_E5,4, NOTE_D5,2, REST,4
+};
+
+song gameofthrones;
+gameofthrones.tempo = 85;
+
+gameofthrones.melody = {
+
+	// Game of Thrones
+	// Score available at https://musescore.com/user/8407786/scores/2156716
+
+	NOTE_G4,8, NOTE_C4,8, NOTE_DS4,16, NOTE_F4,16, NOTE_G4,8, NOTE_C4,8, NOTE_DS4,16, NOTE_F4,16, //1
+	NOTE_G4,8, NOTE_C4,8, NOTE_DS4,16, NOTE_F4,16, NOTE_G4,8, NOTE_C4,8, NOTE_DS4,16, NOTE_F4,16,
+	NOTE_G4,8, NOTE_C4,8, NOTE_E4,16, NOTE_F4,16, NOTE_G4,8, NOTE_C4,8, NOTE_E4,16, NOTE_F4,16,
+	NOTE_G4,8, NOTE_C4,8, NOTE_E4,16, NOTE_F4,16, NOTE_G4,8, NOTE_C4,8, NOTE_E4,16, NOTE_F4,16,
+	NOTE_G4,-4, NOTE_C4,-4,//5
+
+	NOTE_DS4,16, NOTE_F4,16, NOTE_G4,4, NOTE_C4,4, NOTE_DS4,16, NOTE_F4,16, //6
+	NOTE_D4,-1, //7 and 8
+	NOTE_F4,-4, NOTE_AS3,-4,
+	NOTE_DS4,16, NOTE_D4,16, NOTE_F4,4, NOTE_AS3,-4,
+	NOTE_DS4,16, NOTE_D4,16, NOTE_C4,-1, //11 and 12
+
+	//repeats from 5
+	NOTE_G4,-4, NOTE_C4,-4,//5
+
+	NOTE_DS4,16, NOTE_F4,16, NOTE_G4,4, NOTE_C4,4, NOTE_DS4,16, NOTE_F4,16, //6
+	NOTE_D4,-1, //7 and 8
+	NOTE_F4,-4, NOTE_AS3,-4,
+	NOTE_DS4,16, NOTE_D4,16, NOTE_F4,4, NOTE_AS3,-4,
+	NOTE_DS4,16, NOTE_D4,16, NOTE_C4,-1, //11 and 12
+	NOTE_G4,-4, NOTE_C4,-4,
+	NOTE_DS4,16, NOTE_F4,16, NOTE_G4,4,  NOTE_C4,4, NOTE_DS4,16, NOTE_F4,16,
+
+	NOTE_D4,-2,//15
+	NOTE_F4,-4, NOTE_AS3,-4,
+	NOTE_D4,-8, NOTE_DS4,-8, NOTE_D4,-8, NOTE_AS3,-8,
+	NOTE_C4,-1,
+	NOTE_C5,-2,
+	NOTE_AS4,-2,
+	NOTE_C4,-2,
+	NOTE_G4,-2,
+	NOTE_DS4,-2,
+	NOTE_DS4,-4, NOTE_F4,-4,
+	NOTE_G4,-1,
+	
+	NOTE_C5,-2,//28
+	NOTE_AS4,-2,
+	NOTE_C4,-2,
+	NOTE_G4,-2,
+	NOTE_DS4,-2,
+	NOTE_DS4,-4, NOTE_D4,-4,
+	NOTE_C5,8, NOTE_G4,8, NOTE_GS4,16, NOTE_AS4,16, NOTE_C5,8, NOTE_G4,8, NOTE_GS4,16, NOTE_AS4,16,
+	NOTE_C5,8, NOTE_G4,8, NOTE_GS4,16, NOTE_AS4,16, NOTE_C5,8, NOTE_G4,8, NOTE_GS4,16, NOTE_AS4,16,
+	
+	REST,4, NOTE_GS5,16, NOTE_AS5,16, NOTE_C6,8, NOTE_G5,8, NOTE_GS5,16, NOTE_AS5,16,
+	NOTE_C6,8, NOTE_G5,16, NOTE_GS5,16, NOTE_AS5,16, NOTE_C6,8, NOTE_G5,8, NOTE_GS5,16, NOTE_AS5,16,
+};
+	
+
+
+
 
 
 
@@ -324,9 +404,6 @@ void tone(int freq, int time){
 int main (void)
 {
 	init();
-	
-	// change this to make the song slower or faster
-	int tempo = 200;
 		
 	// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
 	// there are two values per note (pitch and duration), so for each note there are four bytes
@@ -359,12 +436,8 @@ int main (void)
 
 			// calculates the duration of each note
 			divider = melody[thisNote + 1];
-			if (divider > 0) {
-				// regular note, just proceed
-				noteDuration = (wholenote) / divider;
-				} else if (divider < 0) {
-				// dotted notes are represented with negative durations!!
-				noteDuration = (wholenote) / abs(divider);
+			noteDuration = (wholenote) / abs(divider);
+			if (divider < 0) {
 				noteDuration *= 1.5; // increases the duration in half for dotted notes
 			}
 
